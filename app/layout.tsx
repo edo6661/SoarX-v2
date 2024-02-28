d05e8d5e2b85dffcbb15466e6087a339d0045efb
Edo <1122140027@global.ac.id>
2024-02-29 04:15:25 +0700

    we will change db to postgre using cockroach db provider, for storage we will be using uploadthing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Providers from "@/providers/Providers";
+import "@uploadthing/react/styles.css";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "./api/uploadthing/core";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,6 +19,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <NextSSRPlugin
+          /**
+           * The `extractRouterConfig` will extract **only** the route configs
+           * from the router to prevent additional information from being
+           * leaked to the client. The data passed to the client is the same
+           * as if you were to fetch `/api/uploadthing` directly.
+           */
+          routerConfig={extractRouterConfig(ourFileRouter)}
+        />
         <Providers>{children}</Providers>
       </body>
     </html>
